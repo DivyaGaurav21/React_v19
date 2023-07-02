dbc9c75ec262816f43836111397f92e700d29c44
Divya Gaurav <divyag.ug16.ee@nitp.ac.in>
2023-07-02 23:50:36 +0530

    add new movie usepopcorn project for practice

--- travel_list/src/components/Item.js
+++ travel_list/src/components/Item.js
@@ -1,18 +1,3 @@
-// export default function Item({ item, onDeleteItem, onToggleItem }) {
-//   return (
-//     <li>
-//       <input
-//         type="checkbox"
-//         value={item.packed}
-//         onChange={() => onToggleItem(item.id)}
-//       />
-//       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-//         {item.quantity} {item.description}
-//       </span>
-//       <button onClick={() => onDeleteItem(item.id)}>❌</button>
-//     </li>
-//   );
-// }
 
 import React from 'react'
 
