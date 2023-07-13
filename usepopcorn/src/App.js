ba3df8079fe236602141dd52f941cfd0a2c471e2
Divya Gaurav <divyag.ug16.ee@nitp.ac.in>
2023-07-13 15:40:10 +0530

    usepopcorn project for practice

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -1,6 +1,12 @@
-import React from "react";
-import Navbar from "./components/Navbar";
+import React, { useState } from "react";
+import Navbar from "./components/navbar/Navbar";
 import Main from "./components/Main";
+import Search from "./components/navbar/Search";
+import Numresults from "./components/navbar/Numresults";
+import Box from "./components/Box";
+import MovieList from "./components/submoviecomp/MovieList";
+import WatchedSummary from "./components/subwatchcomp/WatchedSummary";
+import WatchedMovieList from "./components/subwatchcomp/WatchedMovieList"
 
 const tempMovieData = [
   {
@@ -53,19 +59,28 @@ const tempWatchedData = [
 
 export default function App() {
 
-
+  const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
 
   return (
     <>
-      <Navbar
-        tempMovieData={tempMovieData}
-        tempWatchedData={tempWatchedData}
-      />
+      <Navbar>
+        <Search />
+        <Numresults movies={movies} />
+      </Navbar>
 
-      <Main
-        tempMovieData={tempMovieData}
-        tempWatchedData={tempWatchedData}
-      />
+      <Main>
+        <Box>
+          <MovieList movies={movies} />
+        </Box>
+
+        <Box>
+          <WatchedSummary watched={watched} />
+          <WatchedMovieList watched={watched} />
+        </Box>
+
+
+      </Main>
 
     </>
   );
