d478fed25da94aac841ed56a78799546a251680d
Divya Gaurav <divyag.ug16.ee@nitp.ac.in>
2023-07-17 15:10:13 +0530

    add navbar hero stats and business section

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import styles from "./style"
+import styles from "./styles"
 
 import { Navbar, Billing, CardDeal, Business, Clients, CTA, Stats, Footer, Testimonial, Hero } from "./components"
 
