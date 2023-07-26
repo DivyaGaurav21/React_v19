d9aea8f1a53ff321224b539d052722fe991a1192
Divya Gaurav <divyag.ug16.ee@nitp.ac.in>
2023-07-26 12:17:45 +0530

    add worldwide app for learning react-router-dom library

---
+++ worldwise/src/components/AppLayout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AppNav from './AppNav'
+
+const AppLayout = () => {
+    return (
+        <div>
+            <AppNav />
+            <p>Application</p>
+        </div>
+    )
+}
+
+export default AppLayout
---
+++ worldwise/src/components/AppNav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AppNav = () => {
+    return (
+        <nav>App Navigation</nav>
+    )
+}
+
+export default AppNav
